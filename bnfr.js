221019b22898486b9364df0cee719104ff888ae1
charpie <charpie@ufl.edu>
2018-11-30 13:41:54 -0500

    fixed formatting on bnfr.js

--- bnfr.js
+++ bnfr.js
@@ -25,14 +25,14 @@ const scope         = 'user-read-private user-read-email user-read-playback-stat
 
 //Cookie-Generator
 const generateRandomString = (length) => {
-  var   text     = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var   text     = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
 
-  return text;
+    return text;
 };
 
 //Create server
@@ -65,89 +65,87 @@ app.get('/login', (req, res) => {
 
 //GET: /callback from Spotify with state to get Auth tokens
 app.get('/callback', (req, res) => {
-  const code        = req.query.code || null;
-  const state       = req.query.state || null;
-  const storedState = req.cookies ? req.cookies[stateKey] : null;
+    const code        = req.query.code || null;
+    const state       = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  //Check for the correct state, i.e. compare cookie, and the cookie came back
-  if (state === null || state !== storedState) {
-    res.redirect('/main.html#error=state_mismatch');
-  } 
-  else { //Continue
-    res.clearCookie(stateKey);
+    //Check for the correct state, i.e. compare cookie, and the cookie came back
+    if (state === null || state !== storedState) {
+        res.redirect('/main.html#error=state_mismatch');
+    } 
+    else { //Continue
+        res.clearCookie(stateKey);
 
-    const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+        };
 
-    //Make a POST request to get tokens, then redirect to /main with the tokens as parameters
-    request.post(authOptions, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
+        //Make a POST request to get tokens, then redirect to /main with the tokens as parameters
+        request.post(authOptions, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const access_token  = body.access_token; 
+                const refresh_token = body.refresh_token;
 
-        const access_token  = body.access_token; 
-        const refresh_token = body.refresh_token;
-
-        //Create cookies with the tokens
-        const url = '/main.html';
-        res.cookie(refTokenKey, refresh_token, { secure: false });
-        res.cookie(accTokenKey, access_token,  { secure: false });
-        res.redirect(302, url);
-      } 
-      else { //Failed
-        const url = '/#error=invalid_token';
-        res.redirect(url);
-        console.log('ERROR in callback to main.');
-      }
-    });
-  }
+                //Create cookies with the tokens
+                const url = '/main.html';
+                res.cookie(refTokenKey, refresh_token, { secure: false });
+                res.cookie(accTokenKey, access_token,  { secure: false });
+                res.redirect(302, url);
+            } 
+            else { //Failed
+                const url = '/#error=invalid_token';
+                res.redirect(url);
+                console.log('ERROR in callback to main.');
+            }
+        });
+    }
 });
 
 //GET: /refresh_token used to obtain the new access token with the provided refresh_token
 app.get('/refresh_token', (req, res) => {
-
   //Requesting access token using the refresh token
-  const refresh_token = req.query.refresh_token;
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
+    const refresh_token = req.query.refresh_token;
+    const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        },
+        json: true
+    };
 
-  request.post(authOptions, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const access_token = body.access_token;
-      res.send( {'access_token': access_token} );
-    }
-  });
+    request.post(authOptions, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const access_token = body.access_token;
+            res.send( {'access_token': access_token} );
+        }
+    });
 });
 
 //GET: /avail_dev obtains list of current devices
 app.get('/avail_dev', (req, res) => {
-  const access_token = req.query.access_token;
-  const options = {
-    url: 'https://api.spotify.com/v1/me/player/devices',
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    },
-    json: true
-  };
+    const access_token = req.query.access_token;
+    const options = {
+        url: 'https://api.spotify.com/v1/me/player/devices',
+        headers: {
+            'Authorization': 'Bearer ' + access_token
+        },
+        json: true
+    };
 
-  request.get(options, (error, response, body) => {
-    if (!error && response.statusCode === 200) { res.send(body); }
-  })
+    request.get(options, (error, response, body) => {
+        if (!error && response.statusCode === 200) { res.send(body); }
+    })
 });
 
 
