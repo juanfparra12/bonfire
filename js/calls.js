8f49e02a4c6ea5edd985dd9f0cd2e96432e92771
Gianne <gianneflores18@gmail.com>
2018-12-03 02:07:17 -0500

    added-calls

--- js/calls.js
+++ js/calls.js
@@ -71,19 +71,57 @@ const avail_dev = (access_token, res_func) => {
 
 }
 
-// const search = (access_token, query, res_func) =>{
-//   $.ajax(
-//     {
-//       type: "GET"
-//       url: '/search',
-//         data:{
-//           'access_token': access_token,
-//           'query': query
-//         },
-//         fail: () => { console.log('fail'); }
-//     }
-//   ).done((data) => { res_func(data.query); });
-// }
+const search = (access_token, user_id, res_func) =>{
+$.ajax({
+   url: '/search',
+   data: {
+     access_token: access_token,
+     query: query 
+   },
+   success: (response) => { res_func(response); }
+ });
+}
 
+const create_pl = (access_token, user_id, res_func) =>{
+$.ajax({
+   url: '/create_pl',
+   method: 'POST',
+   data: {
+     access_token: access_token,
+     user_id: user_id 
+   },
+   success: (response) => { res_func(response); }
+ });
+}
 
+const add_track = (access_token, track_uri, res_func) =>{
+$.ajax({
+   url: '/add_track',
+   method: 'POST',
+   data: {
+     access_token: access_token,
+     track_uri: track_uri 
+   },
+   success: (response) => { console.log(response); }
+ });
+}
 
+const start = (access_token, device_id, playlist_uri, res_func) =>{
+$.ajax({
+   url: '/start',
+   method: 'POST',
+   data: {
+     access_token: access_token,
+     device_id: device_id,
+     playlist_uri: playlist_uri 
+   },
+   success: (response) => { console.log(response); }
+ });
+}
+
+var t = null;
+$.getJSON('http://localhost:8080/search?access_token=BQBpppr33JJHQObwpuIgNJuc9jTojgSoHKxLLdRjt11LR140sG8imThbdEPk8ONU0EUu-4zsKCXm3Ncr6Ukd18G_Q-TFG6070KGRJQ4AXTPG6cmFF1DjfixxQEgMUNy3fC41Tofjwu5YfmhlpmWpU6JXxOMnTbJJXZ2NyehJuIjP8Ap-ZagM0i1EoYrVBnEUeRxF0jrbDGZ-c89dn1k7cj1oI-_WR8zueg4nGSyRAoOI92lqLzrlgRHrfgQBE4EzkXIxXVA-bL2rE4ngoONg54hAw_aB6aCoa2g&query=Gold%20Digger'
+  ,function(data){
+    for(var i = 0; i<data.tracks.items.length; i++)
+      alert(data.tracks.items[i].album.name);
+  });
